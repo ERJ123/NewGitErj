9cb3673a6db42a3efa60db31585fed5649878097
beksultanmusaev <musaeviosdev@gmail.com>
2024-04-29 20:43:18 +0600

    Initial Commit

---
+++ NewGitErj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewGitErj
+//
+//  Created by Бексултан Мусаев on 29/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
